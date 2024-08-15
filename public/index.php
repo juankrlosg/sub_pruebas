2747d1c0e2d88322a81b5ed794f064987b8fe19d
FenixNet2021 <136514626+FenixNet2021@users.noreply.github.com>
2024-08-15 16:17:56 -0600

    renew index

--- public/index.php
+++ public/index.php
@@ -31,7 +31,9 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+//require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../sub_pruebas/App_Core/vendor/autoload.php';
+
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +46,9 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+//$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../sub_pruebas/App_core/bootstrap/app.php';
+
 
 $kernel = $app->make(Kernel::class);
 
